8fda639437eedc1c3d2f6f9808922345fd9d7031
unknown <amirrizashibly1382@gmail.com>
2024-02-07 21:39:21 +0330

    The SJF Scheduling Algorithm was added.

---
+++ Scheduling Algorithms/SJF/main.py
@@ -0,0 +1,39 @@
+
+class Process:
+    def __init__(self, number, process_time):
+        self.number = number
+        self.process_time = process_time
+        self.wait_time = 0
+        self.turn_around_time = 0
+
+
+number_of_processes = int(input("Enter the amount of processes: "))
+
+processes = []
+
+for i in range(1, number_of_processes + 1):
+    print(f"Process {i} ")
+    process_time = float(input("Duration: "))
+    processes.append(Process(i, process_time))
+
+processes.sort(key=lambda x: x.process_time)
+
+exit_time = 0
+
+for process in processes:
+    exit_time += process.process_time
+    process.wait_time = exit_time - process.process_time
+    process.turn_around_time = exit_time
+
+average_waiting_time = sum(process.wait_time for process in processes) / number_of_processes
+average_turn_around_time = sum(process.turn_around_time for process in processes) / number_of_processes
+
+print("-----------------------------------------------------------------------------")
+print("| Process   | Waiting Time | Process Time | Turn Around Time                 |")
+print("-----------------------------------------------------------------------------")
+for process in processes:
+    print(f"| process {process.number:<3} | {process.wait_time:<10} | {process.process_time:<10} | {process.turn_around_time:<34}|")
+print("-----------------------------------------------------------------------------")
+
+print("Average Waiting time:", average_waiting_time)
+print("Average Turn Around time:", average_turn_around_time)
